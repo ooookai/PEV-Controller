8797f29d20aba51a7a300faf6f104b413151daa3
ooookai <ooookai@gmail.com>
2018-11-28 14:17:44 -0500

    bind allowed device

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var noble = require('noble')
 
+/*
 function onDiscovery(peripheral) {
-  console.log('onDiscovery', peripheral)
   // peripheral.rssi                             - signal strength
   // peripheral.address                          - MAC address
   // peripheral.advertisement.localName          - device's name
@@ -16,6 +16,34 @@ function onDiscovery(peripheral) {
     JSON.stringify(peripheral.advertisement.manufacturerData)
   )
 }
+*/
+
+// List of allowed devices
+const devices = [
+  "df:58:1a:f4:56:32"
+];
+// last advertising data received
+var lastAdvertising = {
+};
+
+function onDeviceChanged(addr, data) {
+  console.log("Device ",addr,"changed data",JSON.stringify(data));
+}
+
+function onDiscovery(peripheral) {
+    // do we know this device?
+    if (devices.indexOf(peripheral.address)<0) return;
+    // does it have manufacturer data with Espruino/Puck.js's UUID
+    if (!peripheral.advertisement.manufacturerData ||
+        peripheral.advertisement.manufacturerData[0]!=0x90 ||
+        peripheral.advertisement.manufacturerData[1]!=0x05) return;
+    // get just our data
+    var data = peripheral.advertisement.manufacturerData.slice(2);
+    // check for changed services
+    if (lastAdvertising[peripheral.address] != data.toString())
+      onDeviceChanged(peripheral.address, data);
+    lastAdvertising[peripheral.address] = data;
+  }
 
 noble.on('stateChange', function(state) {
   if (state != 'poweredOn') return
