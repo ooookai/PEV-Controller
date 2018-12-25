4eebe13eb660f427c7a6b0d095c0aef30a5b97ad
Unknown <ooookai@gmail.com>
2018-12-25 01:03:09 -0500

    try webrtc live

---
+++ pev_controller/scripts/launch_webcam.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# killall chrome
+google-chrome --allow-running-insecure-content --use-fake-ui-for-media-stream --no-sandbox --no-first-run --ignore-autoplay-restrictions https://appr.tc/r/pev-quick-test
